61c08b6fa5176c4cc718d2ec1cac23d3d9327892
eilon90 <eilon90@gmail.com>
2020-11-24 13:20:59 +0200

    add render

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,4 @@
 $("#button").on('click', function() {
     const ingredient = $("#input").val();
-    $.get(`/recipes/${ingredient}`, function(res){
-        const result = JSON.parse(res).results;
-        const recipes = result.map(r => {return {
-            ingredients: r.ingredients,
-            title: r.title,
-            thumbnail: r.thumbnail,
-            href: r.href
-        }});
-        console.log(recipes);
-    })
+    apiManager.getrecipe(ingredient, renderer.render);
 })
