b8f9b2bf279ed6e478dc0c44ad637a6c99390a75
eilon90 <eilon90@gmail.com>
2020-11-24 13:31:22 +0200

    add dom traversal check

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,10 @@
-$("#button").on('click', function() {
+$('#button').on('click', function() {
     const ingredient = $("#input").val();
     apiManager.getrecipe(ingredient, renderer.render);
+    $("#input").val('');
+})
+
+$('body').on('click', '.image', function() {
+    const firstIn = $(this).closest('.recipe').find('li').first().text();
+    alert(firstIn);
 })
